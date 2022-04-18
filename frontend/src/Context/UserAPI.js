a3ff47c434cc133211933d66fa9cf9d44c2acb0a
Nguyễn Thành Tất 60136813 <63543229+nguyenthanhtat@users.noreply.github.com>
2022-04-18 21:03:21 +0700

    api

--- frontend/src/Context/UserAPI.js
+++ frontend/src/Context/UserAPI.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { GetProfileInitiate,GetAllProductInitiate } from "../redux/Action/ActionAuth";
+import {
+  GetProfileInitiate,
+  GetAllProductInitiate,
+} from "../redux/Action/ActionAuth";
 const UserAPI = (token) => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-      if (token && token.length > 0) {
-        dispatch(GetProfileInitiate(token));
-      }
-    }, [token]);
-  console.log(token,"token")
-    return {};
-  };
-  export default UserAPI;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (token && token.length > 0) {
+      dispatch(GetProfileInitiate(token));
+    }
+  }, [token]);
+  console.log(token, "token");
+  return {};
+};
+
+export default UserAPI;
